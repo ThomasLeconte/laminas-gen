f0f65fe4e956708b288f28d10da3f7f25dc72985
ThomasLeconte <thomasleconte05@gmail.com>
2021-10-31 18:35:40 +0100

    Feat: End of controller generation and module config change

--- Readme.md
+++ Readme.md
@@ -2,8 +2,21 @@
 
 Do you know Symfony's generating commands ? It's the same, for **Laminas Framework** :)
 
-## Use it
+## install it
 Just use `composer install thomasleconte/laminas-gen` !
+
+## Use it
+When library is installed, you can use `vendor/bin/laminas-gen-console <arguments>` to generate items. But if you want, you can create your own command for call this script in `composer.json` :
+
+```json
+    "scripts": {
+        ...
+        "laminas-gen": "vendor/bin/laminas-gen-console",
+        ...
+    }
+
+```  
+Then you will be able to use `composer laminas-gen <arguments>` instead of `vendor/bin/laminas-gen-console <arguments>`.
 
 ##  Debug it
 1 - Run autoload command before work on project : `composer dump-autoload -o`  
--- src/Generators/ControllerGenerator.php
+++ src/Generators/ControllerGenerator.php
@@ -2,7 +2,9 @@
 
 namespace LaminasGen\Generators;
 
+use LaminasGen\Exceptions\ModuleNotFoundException;
 use LaminasGen\Exceptions\EnoughtArgumentsException;
+use LaminasGen\Exceptions\FileAlreadyExistsException;
 
 class ControllerGenerator
 {
@@ -12,26 +14,67 @@ class ControllerGenerator
   public function __construct(array $args)
   {
     $this->args = $args;
-    if (sizeof($args) > $this::$requiredArgs) {
-      echo 'Building "' . $args[1] . '" controller in "' . $args[2] . '" module ...' . "\n";
-      $this->generate();
+    if ($this->checkIfModuleExists()) {
+      if (sizeof($args) > $this::$requiredArgs) {
+        echo 'Building "' . $args[1] . '" controller in "' . $args[2] . '" module ...' . "\n";
+        $this->generate();
+      } else {
+        throw new EnoughtArgumentsException("composer laminas-gen controller <YourControllerName> <YourModuleName>");
+      }
     } else {
-      throw new EnoughtArgumentsException("composer laminas-gen controller <YourControllerName> <YourModuleName>");
+      throw new ModuleNotFoundException($this->getModuleName());
     }
   }
 
   public function generate()
   {
-    $result = file_get_contents(__DIR__ . "/templates/Controller.txt");
+    $result = file_get_contents(__DIR__ . "/templates/controller.txt");
     $result = str_replace("{{controllerName}}", $this->getControllerName(), $result);
     $result = str_replace("{{moduleName}}", $this->getModuleName(), $result);
-    var_dump($result);
     $this->writeInFile($result);
+    $this->updateModuleConfig();
     echo "\n\e[0;30;42mSuccessfull generated " . $this->getControllerName() . " controller in " . $this->getModuleName() . "'s module !\e[0m\n";
   }
 
-  public function writeInFile(string $content){
-    file_put_contents($this->getControllerName().".php", $content);
+  public function writeInFile(string $content)
+  {
+    if ($this->checkIfModuleControllerFolderExists() == false) {
+      var_dump($this->checkIfModuleControllerFolderExists());
+      mkdir("./module/" . $this->getModuleName() . "/src/Controller/", 0777, true);
+      echo "\n\e[1;37;45mController folder not found. Laminas-gen has created it into module/" . $this->getModuleName() . "/src folder.\e[0m\n";
+    }
+    if ($this->checkIfControllerExists()) {
+      throw new FileAlreadyExistsException("./module/" . $this->getModuleName() . "/src/Controller/" . $this->getControllerName() . ".php");
+    } else {
+      file_put_contents("./module/" . $this->getModuleName() . "/src/Controller/" . $this->getControllerName() . ".php", $content);
+    }
+  }
+
+  public function updateModuleConfig()
+  {
+    $moduleConfig = file_get_contents("./module/" . $this->getModuleName() . "/config/module.config.php");
+    $finalControllerName = strtolower(strpos($this->getControllerName(), "Controller") ? 
+      str_replace("Controller", "", $this->getControllerName()) : $this->getControllerName());
+    $configTemplate = file_get_contents(__DIR__ . "/templates/controller-routing-config.txt");
+    $configTemplate = str_replace("{{finalControllerName}}", $finalControllerName, $configTemplate);
+    $configTemplate = str_replace("{{controllerName}}", $this->getControllerName(), $configTemplate);
+    $moduleConfig = str_replace("      'routes' => [", "      'routes' => [\n".$configTemplate, $moduleConfig);
+    file_put_contents("./module/" . $this->getModuleName() . "/config/module.config.php", $moduleConfig);
+  }
+
+  public function checkIfModuleExists(): bool
+  {
+    return file_exists("./module/" . $this->getModuleName() . "/");
+  }
+
+  public function checkIfModuleControllerFolderExists(): bool
+  {
+    return file_exists("./module/" . $this->getModuleName() . "/src/Controller/");
+  }
+
+  public function checkIfControllerExists(): bool
+  {
+    return file_exists("./module/" . $this->getModuleName() . "/src/Controller/" . $this->getControllerName() . ".php");
   }
 
   public function getControllerName(): string
