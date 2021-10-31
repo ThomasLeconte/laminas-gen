42ab2499053867fe84369da92ac1bc109b7f7012
ThomasLeconte <thomasleconte05@gmail.com>
2021-10-31 10:39:04 +0100

    Feat: Start of controller file generation

--- src/Generators/ControllerGenerator.php
+++ src/Generators/ControllerGenerator.php
@@ -6,14 +6,15 @@ use LaminasGen\Exceptions\EnoughtArgumentsException;
 
 class ControllerGenerator
 {
-  public static $requiredArgs = 2;
-  private $args;
+  public static int $requiredArgs = 2;
+  private array $args;
 
   public function __construct(array $args)
   {
     $this->args = $args;
     if (sizeof($args) > $this::$requiredArgs) {
-      echo "Building " . $args[1] . " controller in ". $args[2] ."...\n";
+      echo 'Building "' . $args[1] . '" controller in "' . $args[2] . '" module ...' . "\n";
+      $this->generate();
     } else {
       throw new EnoughtArgumentsException("composer laminas-gen controller <YourControllerName> <YourModuleName>");
     }
@@ -21,6 +22,25 @@ class ControllerGenerator
 
   public function generate()
   {
-    //todo
+    $result = file_get_contents(__DIR__ . "/templates/Controller.txt");
+    $result = str_replace("{{controllerName}}", $this->getControllerName(), $result);
+    $result = str_replace("{{moduleName}}", $this->getModuleName(), $result);
+    var_dump($result);
+    $this->writeInFile($result);
+    echo "\n\e[0;30;42mSuccessfull generated " . $this->getControllerName() . " controller in " . $this->getModuleName() . "'s module !\e[0m\n";
+  }
+
+  public function writeInFile(string $content){
+    file_put_contents("toto.php", $content);
+  }
+
+  public function getControllerName(): string
+  {
+    return $this->args[1];
+  }
+
+  public function getModuleName(): string
+  {
+    return $this->args[2];
   }
 }
--- src/Handler.php
+++ src/Handler.php
@@ -2,6 +2,7 @@
 
 namespace LaminasGen;
 
+use Exception;
 use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 use LaminasGen\Generators\ControllerGenerator;
@@ -14,11 +15,19 @@ class Handler
         echo "Build Handler.php";
     }
 
-    public static function handle(Event $event)
+    public static function handle(Event $event = null, $args = null)
     {
-        $args = $event->getArguments();
-        var_dump($args);
-        switch ($args[0]) {
+        $finalArgs = null;
+        if($event == null){
+            if($args == null){
+                throw new Exception('Please give arguments to command ! Use Laminas-gen like that : composer laminas-gen [module/controller/form] <ModuleName / ControllerName / FormName>');
+            }else{
+                $finalArgs = $args;
+            }
+        }else{
+            $finalArgs = $event->getArguments();
+        }
+        switch ($finalArgs[0]) {
             case "controller":
                 new ControllerGenerator($args);
                 break;
@@ -27,26 +36,26 @@ class Handler
         }
     }
 
-    public static function postUpdate(Event $event)
-    {
-        $composer = $event->getComposer();
-        // do stuff
-    }
+    // public static function postUpdate(Event $event)
+    // {
+    //     $composer = $event->getComposer();
+    //     // do stuff
+    // }
 
-    public static function postAutoloadDump(Event $event)
-    {
-        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-        require $vendorDir . '/autoload.php';
-    }
+    // public static function postAutoloadDump(Event $event)
+    // {
+    //     $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+    //     require $vendorDir . '/autoload.php';
+    // }
 
-    public static function postPackageInstall(PackageEvent $event)
-    {
-        $installedPackage = $event->getOperation()->getPackage();
-        // do stuff
-    }
+    // public static function postPackageInstall(PackageEvent $event)
+    // {
+    //     $installedPackage = $event->getOperation()->getPackage();
+    //     // do stuff
+    // }
 
-    public static function warmCache(Event $event)
-    {
-        // make cache toasty
-    }
+    // public static function warmCache(Event $event)
+    // {
+    //     // make cache toasty
+    // }
 }
